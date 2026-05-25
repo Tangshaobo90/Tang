f7a1b241b94e4603e61aa2958e9a1e1c5d3906bf
Tangshaobo90 <187279980@qq.com>
2026-05-25 20:17:09 +0800

    add number war game page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,29 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: "/portfolio",
+        destination: "/",
+        permanent: true
+      }
+    ];
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/index.html"
+        },
+        {
+          source: "/number-war",
+          destination: "/number-war/index.html"
+        }
+      ]
+    };
   }
 };
 
